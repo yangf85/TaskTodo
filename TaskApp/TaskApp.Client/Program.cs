9b4a534303f68d50c23b83e48c94033ddddcb2a9
锋 杨 <yangf85@163.com>
2024-09-17 20:05:14 +0800

    修改

--- TaskApp/TaskApp.Client/Program.cs
+++ TaskApp/TaskApp.Client/Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using TaskApp.Client.Services;
 
 namespace TaskApp.Client;
 
-class Program
+internal class Program
 {
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         builder.Services.AddMudServices();
-
+        builder.Services.AddScoped<TaskService>();
 
         await builder.Build().RunAsync();
     }
